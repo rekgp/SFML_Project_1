e2efc89a8d9773f5e75e76209d2761e98da82287
rek_Lap <rekgamerpro@gmail.com>
2022-06-19 22:56:26 +0200

    Paticle implementation

--- Particle.cpp
+++ Particle.cpp
@@ -1,1 +1,40 @@
 #include "Particle.h"
+
+void Particle::setParticleDrawing(bool _drawPart)
+{
+	drawPart = _drawPart;
+}
+
+void Particle::setTrailDrawing(bool _drawTrail)
+{
+	drawTrail = _drawTrail;
+}
+
+void Particle::setParticleShape(const sf::Shape& _shape)
+{
+	*shape = _shape;
+}
+
+void Particle::setFlowField(const flowField& _flow)
+{
+	field = _flow;
+}
+
+void Particle::update(sf::Time elapsed)
+{
+	vel = vel + math(field.getVelocityVector(pos));
+	pos = pos + vel * elapsed.asSeconds();
+	shape->setPosition(sf::Vector2f(pos.x, pos.y));
+
+}
+
+void Particle::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	if (drawPart) {
+		target.draw(*shape);
+	}
+	if (drawTrail) {
+		target.draw(trail);
+	}
+
+}
--- Particle.h
+++ Particle.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "Vec3.h"
+#include "Vec2.h"
 #include "flowField.h"
 
 class Particle :
@@ -13,13 +14,15 @@ public:
     void setParticleDrawing(bool _drawPart);
     void setTrailDrawing(bool _drawTrail);
     void setParticleShape(const sf::Shape& _shape);
-    virtual void update(const flowField &_flowfield);
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
-
+    void setFlowField(const flowField& _flow);
+    void update(sf::Time elapsed);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    virtual Vec2f math(const Vec2f &_vec);
 
 protected:
     Vec2<float> pos, vel;
     bool drawPart, drawTrail;
     sf::Shape* shape;
     sf::VertexArray trail;
+    flowField& field;
 };
